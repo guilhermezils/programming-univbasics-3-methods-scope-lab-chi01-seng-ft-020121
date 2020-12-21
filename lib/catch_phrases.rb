c43fe508ba51a0a61f85167fd71fd86e0d429a26
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2020-12-21 18:14:22 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,5 +7,17 @@ def mario
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link
+  phrase = "It's Dangerous To Go Alone!"
+  puts phrase
+end
+
+def all_phrases
+  puts mario
+  puts toadstool
+  puts link
 end
